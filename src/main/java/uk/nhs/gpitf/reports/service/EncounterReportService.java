f97ce6f6e13dc7b514bfd72f476233defe7a4e70
Charlie Marcus <charlie.marcus1@nhs.net>
2020-03-24 10:27:07 +0000

    Feature/ncth 587 ui report input

--- src/main/java/uk/nhs/gpitf/reports/service/EncounterReportService.java
+++ src/main/java/uk/nhs/gpitf/reports/service/EncounterReportService.java
@@ -1,22 +1,35 @@
 package uk.nhs.gpitf.reports.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.xmlbeans.XmlException;
 import org.hl7.fhir.dstu3.model.Encounter;
 import org.hl7.fhir.dstu3.model.Reference;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import uk.nhs.connect.iucds.cda.ucr.ClinicalDocumentDocument1;
 import uk.nhs.gpitf.reports.transform.EncounterTransformer;
+import uk.nhs.itk.envelope.DistributionEnvelopeDocument;
 
 @Service
 @RequiredArgsConstructor
 public class EncounterReportService {
+
+  private static final String CLINICAL_DOCUMENT_NODE_NAME = "ClinicalDocument";
 
   private final EncounterTransformer encounterTransformer;
 
   private final FhirStorageService storageService;
   private final CarePlanService carePlanService;
 
-  public Reference createEncounterReport(ClinicalDocumentDocument1 document) {
+  public Reference createEncounterReport(String xmlReportString) throws XmlException {
+
+    ClinicalDocumentDocument1 document;
+
+    DistributionEnvelopeDocument envelopedDocument =
+        DistributionEnvelopeDocument.Factory.parse(xmlReportString);
+
+    document = ClinicalDocumentDocument1.Factory.parse(findClinicalDoc(envelopedDocument));
 
     Encounter encounter = encounterTransformer.transform(document);
     Reference encounterRef = storageService.create(encounter);
@@ -24,6 +37,24 @@ public class EncounterReportService {
     carePlanService.createCarePlans(document, encounterRef);
 
     return encounterRef;
+  }
+
+  private Node findClinicalDoc(DistributionEnvelopeDocument envelopedDocument)
+      throws XmlException {
+    NodeList childNodes = envelopedDocument.getDistributionEnvelope()
+        .getPayloads()
+        .getPayloadArray(0)
+        .getDomNode()
+        .getChildNodes();
+
+    for (int i=0; i < childNodes.getLength(); i++) {
+      Node node = childNodes.item(i);
+
+      if (node.getNodeName().contains(CLINICAL_DOCUMENT_NODE_NAME)) {
+        return node;
+      }
+    }
+    throw new XmlException("No clinical document found in Envelope");
   }
 
 }

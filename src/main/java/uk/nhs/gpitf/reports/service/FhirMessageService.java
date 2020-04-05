9260f6575a4cdf9638ff4aee39013e72ca0b6cce
Charlie Marcus <charlie.marcus1@nhs.net>
2020-04-05 23:21:21 +0000

    Feature/ncth 626 message header

---
+++ src/main/java/uk/nhs/gpitf/reports/service/FhirMessageService.java
@@ -0,0 +1,58 @@
+package uk.nhs.gpitf.reports.service;
+
+import static uk.nhs.gpitf.reports.util.ReferenceUtil.ofType;
+
+import java.util.Date;
+import java.util.UUID;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.dstu3.model.Bundle.BundleType;
+import org.hl7.fhir.dstu3.model.ContactPoint;
+import org.hl7.fhir.dstu3.model.ContactPoint.ContactPointSystem;
+import org.hl7.fhir.dstu3.model.ContactPoint.ContactPointUse;
+import org.hl7.fhir.dstu3.model.Encounter;
+import org.hl7.fhir.dstu3.model.MessageHeader;
+import org.hl7.fhir.dstu3.model.MessageHeader.MessageDestinationComponent;
+import org.hl7.fhir.dstu3.model.MessageHeader.MessageSourceComponent;
+import org.hl7.fhir.dstu3.model.Reference;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.nhs.gpitf.reports.enums.MessageEvent;
+
+@Service
+public class FhirMessageService {
+
+  @Value("${gpfit.service}")
+  private String thisService;
+
+  public Bundle createMessage(Bundle bundle) {
+
+    MessageHeader messageHeader = new MessageHeader()
+        .setEvent(MessageEvent.COMMUNICATION_REQUEST.toCoding()) //TODO: Find a code?
+        .addDestination(new MessageDestinationComponent()
+          .setName("Test Destination")
+          .setEndpoint("test.endpoint"))
+        .setTimestamp(new Date())
+        .setSource(new MessageSourceComponent()
+          .setName("EMS GPFIT Transformation Service")
+          .setContact(new ContactPoint()
+            .setSystem(ContactPointSystem.EMAIL)
+            .setValue("email@address.com")
+            .setUse(ContactPointUse.WORK))
+          .setEndpoint(thisService));
+    messageHeader.setId(UUID.randomUUID().toString());
+
+    bundle.getEntry().stream()
+        .map(BundleEntryComponent::getFullUrl)
+        .map(Reference::new)
+        .filter(ofType(Encounter.class))
+        .forEach(messageHeader::addFocus);
+
+    bundle.setType(BundleType.MESSAGE);
+    bundle.getEntry().add(0, new BundleEntryComponent()
+      .setResource(messageHeader));
+
+    return bundle;
+  }
+
+}
